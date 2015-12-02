a675a4819b9169e967f3ed565cb55dcf5b1ef76d
Aulide81 <emiliojat@gmail.com>
2015-12-02 12:35:36 +0100

    Update descriptivos13.R

--- R/descriptivos13.R
+++ R/descriptivos13.R
@@ -1401,6 +1401,7 @@ addvallab<-gtools::defmacro(df,var,val,
                               if (!is.numeric(val)) stop("El vector ha de ser numerico")
                               if (length(names(val)) == 0) stop("La variable def se ha introducido sin etiquetas")
                               if (length(unique(val)) != length(val)) stop("valores repetidos")
+                              val<-sort(val)
                               a<-attr(df$var,"val.lab")
                               if (length(a[a%in%val])>0) names(a)[a[a%in%val]]<-names(val)[val%in%a[a%in%val]]
                               attr(df$var,"val.lab")<-sort(c(a,val[!val%in%a[a%in%val]]))
